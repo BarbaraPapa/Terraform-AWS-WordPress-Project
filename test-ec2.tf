bd08e94fb62c160f5ef2151566bb8b840337feb5
BarbaraPapa <103266205+BarbaraPapa@users.noreply.github.com>
2025-02-11 11:20:43 +0100

    add configuration for terraform cloud

--- test-ec2.tf
+++ test-ec2.tf
@@ -1,13 +1,24 @@
+terraform {
+  backend "remote" {
+    organization = "kuboski2025"
+
+    workspaces {
+      name = "AWS-WordPress-Project"
+    }
+  }
+}
+
+
 provider "aws" {
   region = "us-west-2"  
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "wordpress" {
   ami           = "ami-0a897ba00eaed7398" 
   instance_type = "t2.micro"
   key_name      = "my-key-pair"  
 
   tags = {
-    Name = "MyInstance"
+    Name = "WordPressInstance"
   }
 }
