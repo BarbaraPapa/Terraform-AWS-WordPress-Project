602be632c6fa194276d59fdbf3676c8d41f375e3
BarbaraPapa <103266205+BarbaraPapa@users.noreply.github.com>
2025-02-11 13:17:36 +0100

    add VPC and Subnet

--- test-ec2.tf
+++ test-ec2.tf
@@ -12,16 +12,40 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region  = "us-west-2"
   profile = "default"
 }
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MainVPC"
+  }
+}
+
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "MainSubnet"
+  }
+}
+
 
 resource "aws_instance" "wordpress" {
   ami           = "ami-0a897ba00eaed7398"
   instance_type = "t2.micro"
   key_name      = "my-key-pair"
+  subnet_id     = aws_subnet.main_subnet.id
 
   tags = {
     Name = "WordPressInstance"
   }
 }
+
