8ad33edddbcc761b0bb550555116549cdfdfe75d
BarbaraPapa <103266205+BarbaraPapa@users.noreply.github.com>
2025-02-11 12:44:03 +0100

    add configuration for using terraform localy

--- test-ec2.tf
+++ test-ec2.tf
@@ -1,22 +1,25 @@
 terraform {
-  backend "remote" {
-    organization = "kuboski2025"
-
-    workspaces {
-      name = "AWS-WordPress-Project"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
+
+  backend "local" {
+    path = "./terraform.tfstate"
   }
 }
 
-
 provider "aws" {
-  region = "us-west-2"  
+  region = "us-west-2"
+  profile = "default"
 }
 
 resource "aws_instance" "wordpress" {
-  ami           = "ami-0a897ba00eaed7398" 
+  ami           = "ami-0a897ba00eaed7398"
   instance_type = "t2.micro"
-  key_name      = "my-key-pair"  
+  key_name      = "my-key-pair"
 
   tags = {
     Name = "WordPressInstance"
